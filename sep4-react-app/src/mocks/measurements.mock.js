afbe5a337fd01426b254640dd807791349109dbd
Daniel Ziola <danielmaks400@gmail.com>
2026-04-23 22:08:54 +0200

    pc-home-page: added mocking directory and finished mocking room

---
+++ sep4-react-app/src/mocks/measurements.mock.js
@@ -0,0 +1,6 @@
+export const measurementsByRoom = {room, roomId}
+temperature={value:int, unit:char};
+humidity = {value:int, unit:char};
+light={value:int, unit:String};
+timeStamp = new Date();
+room ={roomId:int, temperature, humidity, light, timeStamp};
---
+++ sep4-react-app/src/mocks/rooms.mock.js
@@ -0,0 +1,4 @@
+export const rooms = [
+  { id: "room-1", name: "Living Room" },
+  { id: "room-2", name: "Bedroom" },
+];
