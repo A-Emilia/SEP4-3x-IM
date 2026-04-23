39fe4344e7889cca74dbb03b1fd77c40661ca545
Daniel Ziola <danielmaks400@gmail.com>
2026-04-23 22:16:24 +0200

    pc-home-page: fix - error with wrong unit mocked for light % instead of lx

--- sep4-react-app/src/mocks/measurements.mock.js
+++ sep4-react-app/src/mocks/measurements.mock.js
@@ -3,12 +3,12 @@ export const measurementsByRoom = {
         roomId: 1,
         temperature: {value:18, unit:"C", timeStamp: new Date()},
         humidity: {value:40, unit:"%", timeStamp: new Date()},
-        light: {value:40, unit:"%", timeStamp: new Date()} 
+        light: {value:40, unit:"lx", timeStamp: new Date()} 
     },
     2 : {
         roomId: 2,
         temperature: {value:14, unit:"C", timeStamp: new Date()},
         humidity: {value:54, unit:"%", timeStamp: new Date()},
-        light: {value:70, unit:"%", timeStamp: new Date()} 
+        light: {value:70, unit:"lx", timeStamp: new Date()} 
     },
 };
