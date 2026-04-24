30a4e373ed40c59415eb523b99dc6120dfc3a50a
Daniel Ziola <danielmaks400@gmail.com>
2026-04-24 13:12:07 +0200

    pc-home-page: added unit test for happy case valid room Id

--- sep4-react-app/src/test/measurementsService.test.js
+++ sep4-react-app/src/test/measurementsService.test.js
@@ -1,2 +1,21 @@
-import "@testing-library/jest-dom";
+import {describe, it, expect} from "vitest";
+import { fetchCurrentMeasurements } from "../services/measurementsService";
 
+describe("fetchCurrentMeasurements", () => 
+{
+    //happy scenario - valid room ID shows right measurements 
+    it("resolves with measurements when given a valid roomId",
+       async () =>  {
+        const result = await fetchCurrentMeasurements(1);
+        expect(result.roomId).toBe(1);
+        expect(result.temperature.value).toBe(18);
+        expect(result.temperature.unit).toBe("C");
+        expect(result.humidity.value).toBe(40);
+         expect(result.humidity.unit).toBe("%");
+        expect(result.light.value).toBe(40);
+        expect(result.light.unit).toBe("lx");
+       }
+    
+    )
+}
+)
