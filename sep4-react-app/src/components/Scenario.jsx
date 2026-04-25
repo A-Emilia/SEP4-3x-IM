b41b9c7dc926d7af151ccec188eb3b33ce8b0323
Smarties <355510@via.dk>
2026-04-25 14:48:12 +0200

    poc-comfort-zone scenario final+mocking,  feedback final+mocking

--- sep4-react-app/src/components/Scenario.jsx
+++ sep4-react-app/src/components/Scenario.jsx
@@ -1,20 +1,21 @@
-function Scenario({ roomID, date, onLiked, onDisliked }) {
+function Scenario({ scenario, onLiked, onDisliked, disabled }) {
   return (
     <div className="scenario">
-        <div>
-            <h3>Room #{roomID}</h3>
-            <p>Date: {date}</p>
-        </div>
-        <div>
-            <button className="nav-btn" onClick={onLiked}>
-                Like
-            </button>
-            <button className="nav-btn" onClick={onDisliked}>
-                Dislike
-            </button>
-        </div>
+      <div>
+        <h3>Room #{scenario.roomId}</h3>
+        <p>Date: {scenario.date}</p>
+
+        <p>Temperature: {scenario.temperature}°C</p>
+        <p>Humidity: {scenario.humidity}%</p>
+        <p>Light: {scenario.light}</p>
+      </div>
+
+      <div>
+        <button className="nav-btn" onClick={onLiked} disabled={disabled}>Like</button>
+        <button className="nav-btn" onClick={onDisliked} disabled={disabled}>Dislike</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Scenario
+export default Scenario;
